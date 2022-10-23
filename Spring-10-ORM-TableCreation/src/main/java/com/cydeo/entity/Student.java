fde72dd0140a98c14abb98cf9a5e4ab017ad3ce0
omerk <omerkarahan1989@gmail.com>
2022-10-23 17:10:22 -0400

    Annotations Added

--- Spring-10-ORM-TableCreation/src/main/java/com/cydeo/entity/Student.java
+++ Spring-10-ORM-TableCreation/src/main/java/com/cydeo/entity/Student.java
@@ -1,14 +1,35 @@
 package com.cydeo.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import com.cydeo.enums.Gender;
+
+import javax.persistence.*;
+import java.time.LocalDate;
 
 @Entity
+@Table(name = "students")
 public class Student {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    private String firstName;  //forst_name
+
+    @Column(name = "studentFirstName")
+    private String firstName;  //first_name
+    @Column(name = "studentLastName")
     private String lastName;
     private String email;
+
+    @Transient
+    private String city;
+
+    @Column(name = "DATE")
+    private LocalDate birthdate;
+    @Column(name = "TIME")
+    private LocalDate birthTime;
+    @Column(name = "TIMESTAMP")
+    private LocalDate birthDateTime;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
 }
